516c6826dc2a73d0379ed37d105a73857748e515
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-02-25 16:29:38 +0530

    Update main.tf

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -90,28 +90,6 @@ resource "aws_iam_role" "ecs_execution_role" {
       }
     ]
   })
-
-  tags = {
-    Name = "ecs-execution-role"
-  }
-}
-
-# ECS Task Role
-resource "aws_iam_role" "ecs_execution_role" {
-  name = "ecs-execution-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action    = "sts:AssumeRole"
-        Effect    = "Allow"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-      }
-    ]
-  })
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {

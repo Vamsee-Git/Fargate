c4587f2c4a4725f931b5db5245d47a2b18326b09
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-02-25 16:40:13 +0530

    Update main.tf

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -96,7 +96,27 @@ resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
   role       = aws_iam_role.ecs_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+# ECS Task Role
+resource "aws_iam_role" "ecs_task_role" {
+  name = "ecs-task-role"
 
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Name = "ecs-task-role"
+  }
+}
 # Create Route Table for Public Subnets and associate it
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id

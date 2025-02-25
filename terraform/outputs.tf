0abb63e9dc97299ffb6080887dd3ef77a2b6e81b
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-02-25 16:34:31 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -14,9 +14,8 @@ output "alb_dns_name" {
   description = "The DNS name of the Application Load Balancer"
   value       = module.alb.alb_dns_name
 }
-/*
+
 output "patient_service_repo_url" {
   description = "The repository URL for the patient service"
   value       = module.ecr.patient_service_repo_url
 }
-*/
